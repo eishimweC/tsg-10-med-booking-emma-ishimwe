3cd3020c149c3293a47f05d7e7660ad8cd5d7811
eishimweC <EIshimweC@YorkSolutions.net>
2024-12-12 22:43:41 -0500

    Adding Dockerfile spring_boot_backend postgress_db react_frontend

--- medical/src/main/java/com/york/medical/MedicalApplication.java
+++ medical/src/main/java/com/york/medical/MedicalApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedicalApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello World");
 		SpringApplication.run(MedicalApplication.class, args);
 	}
 
--- medical/src/main/java/com/york/medical/appointment/AppointmentController.java
+++ medical/src/main/java/com/york/medical/appointment/AppointmentController.java
@@ -1,11 +1,9 @@
 package com.york.medical.appointment;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(path= "/api/appointments")
